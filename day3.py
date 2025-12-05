d3ef388efabac9cbda63252dad6092d534cdee41
hex4 <121138331+Hex-4@users.noreply.github.com>
2025-12-04 18:57:23 -0700

    day 3 part 2 (late)

--- day3.py
+++ day3.py
@@ -1,4 +1,4 @@
-# AoC 02/12/2025
+# AoC 03/12/2025
 import time
 import re 
 
@@ -8,34 +8,25 @@ start = time.time()
 with open("inputs/3.txt") as file:
     lines = [line.rstrip() for line in file]
 
-def find_bit(line: str, digit: str):
-    if digit in line:
-        i = line.index(digit)
-        if i + 1 == len(line):
-            return
-        return line[i:]
-    return
 
-def solve_last_bit(line: str):
-    for digit in reversed(range(1,10)):
-        if not str(digit) in line[1:]:
-            continue
-        return int(line[0] + str(digit))
-    return
+            
+
 
 for line in lines:
-    print(line)
+    budget = len(line) - 12
+    
+    stack = []
 
-    for digit in reversed(range(1,10)): # 1-9
-        next_bit = find_bit(line, str(digit))
-        if next_bit:
-            joltage = solve_last_bit(next_bit)
-            if joltage:
-                solution += joltage
-                print(joltage)
-                break
+    for digit in line:
+        while stack and budget > 0 and int(stack[-1]) < int(digit):
+            stack.pop()
+            budget -= 1
+        stack.append(digit)
 
+    stack = stack[:12]
+    joltage = int("".join(stack))
 
+    solution +=joltage
 print("SOLUTION: " + str(solution))
 
 print("SOLUTION TOOK: " + str(time.time() - start))
